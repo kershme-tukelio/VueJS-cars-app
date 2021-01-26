67bb5a318c539624c343571e4776def2dc35c0f4
Jovan Zoric <jzjovanzoric@gmail.com>
2021-01-26 20:23:43 +0100

    Objective 5 complete

--- src/views/AddCar.vue
+++ src/views/AddCar.vue
@@ -1,17 +1,17 @@
 <template>
   <form @submit.prevent="onSubmit">
     <div class="mb-3">
-      <label for="brand" class="form-label">Brand</label>
-      <input v-model="formData.brand" type="text" class="form-control" id="brand" aria-describedby="brand"/>
+      <label for="brand" class="form-label">Brand </label>
+      <input required v-model="formData.brand" type="text" class="form-control" id="brand" aria-describedby="brand"/>
     </div>
 
     <div class="mb-3">
-      <label for="model" class="form-label">Model</label>
-      <input v-model="formData.model" type="text" class="form-control" id="model" aria-describedby="model"/>
+      <label for="model" class="form-label">Model </label>
+      <input required v-model="formData.model" type="text" class="form-control" id="model" aria-describedby="model"/>
     </div>
 
     <div class="mb-3">
-      <label for="year" class="form-label">Year</label>
+      <label for="year" class="form-label">Year </label>
       <select v-model="formData.year">
         <option v-for="year in yearRange" :key="year">
           {{year}}
@@ -21,11 +21,11 @@
 
     <div class="mb-3 form-check">
       <label class="form-label" for="max-speed">Max Speed </label>
-      <input min=0 v-model="formData.maxSpeed" type="number" class="form-number-input" id="max-speed">
+      <input required min=0 v-model="formData.maxSpeed" type="number" class="form-number-input" id="max-speed">
     </div>
     <div class="mb-3 form-check">
       <label class="form-label" for="doors">Number Of Doors </label>
-      <input min=0 v-model="formData.numberOfDoors" type="number" class="form-number-input" id="doors">
+      <input required min=0 v-model="formData.numberOfDoors" type="number" class="form-number-input" id="doors">
     </div>
     <div class="mb-3 form-check">
       <label class="form-label" for="doors">Automatic </label>
@@ -34,7 +34,7 @@
 
       <div class="btn-group btn-group-toggle" data-toggle="buttons">
         <label class="btn btn-secondary">
-          <input v-model="formData.engine" type="radio" name="engine" value="diesel" id="diesel" autocomplete="off" checked> Diesel
+          <input v-model="formData.engine" type="radio" name="engine" value="diesel" id="diesel" autocomplete="on" checked> Diesel
         </label>
         <label class="btn btn-secondary">
           <input v-model="formData.engine" type="radio" name="engine" value="petrol" id="petrol" autocomplete="off"> Petrol
@@ -46,8 +46,9 @@
           <input v-model="formData.engine" type="radio" name="engine" value="hybrid" id="hybrid" autocomplete="off"> Hybrid
         </label>
       </div><br/><br/>
-    <button type="reset" > Reset form </button><br/><br/>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button @click="preview()" type="button" class="btn btn-primary"> Preview form </button>
+    <button type="reset" class="btn btn-primary"> Reset form </button> <br/><br/>
+    <button type="submit" class="btn btn-primary"> Submit </button>
   </form>
 </template>
 
@@ -59,6 +60,17 @@ export default {
     async onSubmit(){
       await carService.add(this.formData)
       this.$router.push('/cars');
+    },
+    preview() {
+      alert(
+        [this.formData.brand, 
+        this.formData.model, 
+        this.formData.year, 
+        this.formData.numberOfDoors, 
+        this.formData.maxSpeed, 
+        this.formData.isAutomatic, 
+        this.formData.engine,] 
+      )
     }
   },
   data(){
